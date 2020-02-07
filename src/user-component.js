5d8e08378e585a3922d6289d661eef73a99db071
Deadlock <poghosyan26@gmail.com>
2020-02-07 23:22:04 +0400

    responsive

--- src/user-component.js
+++ src/user-component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.css';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { makeStyles, fade } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
@@ -9,10 +9,11 @@ import MenuIcon from '@material-ui/icons/Menu';
 import image from './user-male.jpg';
 import Drawer from '@material-ui/core/Drawer';
 import EditIcon from '@material-ui/icons/Edit';
-import Popover from '@material-ui/core/Popover';
 import TextField from '@material-ui/core/TextField';
 import LockIcon from '@material-ui/icons/Lock';
 import LanguageIcon from '@material-ui/icons/Language';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
 import {useRef} from 'react';
 import {
   BrowserRouter as Router,
@@ -21,9 +22,61 @@ import {
   Link
 } from "react-router-dom";
 import { Dialog } from '@material-ui/core';
-
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 200,
+      '&:focus': {
+        width: 300,
+      },
+    },
+  },
+}));
 
 function TaskBar() {
+  const classes =useStyles()
   const [state,Setstate]=React.useState(false);
   const toggleDrawer=(e)=> event => Setstate(e)
     return (
@@ -32,39 +85,54 @@ function TaskBar() {
       <AppBar position="static" >
         <Toolbar>
           <div className="menu">
-        <IconButton  onClick={ toggleDrawer(true)}ge="start"  color="inherit" aria-label="menu">
+        <IconButton style={{padding:"0 10px 0 0"}}  onClick={ toggleDrawer(true)}ge="start"  color="inherit" aria-label="menu">
+          
       <MenuIcon />
-
+        
     </IconButton>
+    <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase 
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          
     <Drawer open={state} onClose={toggleDrawer(false)}>
-      <div>
-    <Link  style={{ color:'white', textDecoration: 'none' }} to="/">  <Button color="primary" >
+     <div className="drawer">
+    <Link  style={{ color:'white', textDecoration: 'none' }} to="/">  <Button onClick={toggleDrawer(false)} fullWidth color="primary" >
             Main
               </Button></Link>
-              </div>
-              <div>
-              <Link style={{  color:'white', textDecoration: 'none' }} to="/users">    <Button color="primary" >
+              
+             
+              <Link style={{  color:'white', textDecoration: 'none' }} to="/users">    <Button onClick={toggleDrawer(false)} fullWidth color="primary" >
             User
               </Button> </Link>
-              </div>
-              <div>
-              <Link  style={{ color:'white', textDecoration: 'none' }} to="/">  <Button color="primary" >
+              
+             
+              <Link  style={{ color:'white', textDecoration: 'none' }} to="/">  <Button onClick={toggleDrawer(false)} fullWidth color="primary" >
             Exams
               </Button></Link>
-              </div>
-              <div>
-              <Link  style={{ color:'white', textDecoration: 'none' }} to="/">  <Button color="primary" >
+              
+             
+              <Link  style={{ color:'white', textDecoration: 'none' }} to="/">  <Button onClick={toggleDrawer(false)} fullWidth color="primary" >
             Contact
               </Button></Link>
-              </div>
-              <div>
-              <Link  style={{ color:'white', textDecoration: 'none' }} to="/">  <Button color="primary" >
+              
+             
+              <Link  style={{ color:'white', textDecoration: 'none' }} to="/">  <Button onClick={toggleDrawer(false)} fullWidth color="primary" >
             Logout
               </Button></Link>
               </div>
       </Drawer>
     </div>
-
+         <div className="navbar-menu">
         <Link style={{ color:'white', textDecoration: 'none' }} to="/">  <Button color="inherit" >
             Main
               </Button></Link>
@@ -80,9 +148,23 @@ function TaskBar() {
             Contact
                   </Button>
                   <div className="spacer"></div>
+                  <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
           <Button color="inherit" >
             Logout
                   </Button>
+                  </div>
         </Toolbar>
       </AppBar>
       <Switch>
